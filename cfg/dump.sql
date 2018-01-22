0ecb92ee69d543cd93529010198b80441c36f4f6
Vitaly Kiselev <st411ar@ya.ru>
2018-01-22 05:20:33 +0300

    web api v0.1.1, added book.readerId

--- cfg/dump.sql
+++ cfg/dump.sql
@@ -37,8 +37,10 @@ CREATE TABLE books (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL,
   ownerId INT NOT NULL,
+  readerId INT,
   PRIMARY KEY (id),
-  FOREIGN KEY (ownerId) REFERENCES users(id)
+  FOREIGN KEY (ownerId) REFERENCES users(id),
+  FOREIGN KEY (readerId) REFERENCES users(id)
 ) ENGINE = INNODB;
 
 -- ---
@@ -50,7 +52,12 @@ INSERT INTO users (name) VALUES ('Алексей Киселёв');
 INSERT INTO users (name) VALUES ('Виталий Киселёв');
 INSERT INTO users (name) VALUES ('Татьяна Ковальчук');
 INSERT INTO users (name) VALUES ('Алексей Пташник');
+INSERT INTO users (name) VALUES ('Артём Терехович');
 
-INSERT INTO books (name, ownerId) VALUES ('Экстремальное программирование. TDD', 3);
-INSERT INTO books (name, ownerId) VALUES ('Совершенный код', 3);
-INSERT INTO books (name, ownerId) VALUES ('Философия JAVA', 2);
+INSERT INTO books (name, ownerId, readerId) VALUES ('Экстремальное программирование. TDD', 3, 1);
+INSERT INTO books (name, ownerId, readerId) VALUES ('Совершенный код', 3, 4);
+INSERT INTO books (name, ownerId) VALUES ('Структуры данных и алгоритмы java', 3);
+INSERT INTO books (name, ownerId) VALUES ('Приёмы объектно-ориентированного проектирования', 3);
+INSERT INTO books (name, ownerId, readerId) VALUES ('Философия JAVA', 2, 5);
+INSERT INTO books (name, ownerId, readerId) VALUES ('Трудно быть Богом', 6, 3);
+INSERT INTO books (name, ownerId, readerId) VALUES ('Основы data science', 6, 3);
