f683dbffe27a3aa94ecfc9476b5a3c452fdf8575
Vitaly Kiselev <st411ar@ya.ru>
2018-01-22 06:42:42 +0300

    web api v0.1.2, added books.reader

--- custom/web-api/src/IO.Swagger/Data/PaperLibContext.cs
+++ custom/web-api/src/IO.Swagger/Data/PaperLibContext.cs
@@ -7,5 +7,12 @@ namespace IO.Swagger.Data {
 
 		public DbSet<User> Users { get; set; }
 		public DbSet<Book> Books { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Book>()
+					.HasOne(b => b.Owner)
+					.WithMany(u => u.Books);
+		}
 	}
 }
