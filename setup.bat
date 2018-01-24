75e2fd481941f73b3fdf7340952c3c46b0d614e3
Vitaly Kiselev <st411ar@ya.ru>
2018-01-24 14:36:33 +0300

    api v0.3.3, improved sign in operation

--- setup.bat
+++ setup.bat
@@ -12,7 +12,7 @@ set mysql-dump-path=%dir-cfg%/dump.sql
 
 :: web-api
 set swagger-codegen-cli-jar=swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar
-set swagger-descriptor-path=%dir-cfg%/paperlib_0.3.2_swagger.json
+set swagger-descriptor-path=%dir-cfg%/paperlib_0.3.3_swagger.json
 set web-api-name=web-api
 set src=src/IO.Swagger
 
